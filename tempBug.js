1cb3aadf6a3f726a72ee3e20d7d4d74f72883bf2
Randolph7454 <43218196+Randolph7454@users.noreply.github.com>
2018-10-19 14:44:54 -0500

    Update tempBug.js

--- tempBug.js
+++ tempBug.js
@@ -3,22 +3,21 @@ function tempBug()
     var temp = Number(document.Temperature.input1.value);
     if (temp > 80)
     {
-        document.getElementById('weather').src = "img/summer.jpg";
+        document.getElementById('weather').src="img/Summer.jpg";
         return "Summer";
     }
     else if (temp > 65)
     {
-        document.getElementById('weather').src = "img/Spring-picture.jpg";
+        document.getElementById('weather').src="img/Spring.jpg";
         return "Spring";
     }
     else if (temp > 40)
 {
-    document.getElementById('weather').src = "img/fall.jpeg";
+    document.getElementById('weather').src="img/Fal.jpg";
  return "Fall";
 }
 else
-    {
-        document.getElementById('weather').src = "img/wnter.jpg";
+    {  document.getElementById('weather').src="img/Winter.jpg";
         return "Winter";
     }
 }
